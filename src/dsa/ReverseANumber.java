24a0afba39515b0777ec77f825c99eb0ec3fdd30
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2021-12-21 18:52:58 +0530

    Reversing A Number

---
+++ src/dsa/ReverseANumber.java
@@ -0,0 +1,19 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number to be reversed");
+        int num = in.nextInt();
+        int ans=0;
+
+        while (num>0){
+            int rem = num%10;
+            ans = ans * 10 + rem;
+            num = num/10;
+        }
+        System.out.println(ans);
+    }
+}
